fb8fb3e2c1afa9a3976d456ddb7b5630f594ae3f
Alec Burgdorf <aeburgd@gmail.com>
2020-02-14 16:51:48 -0600

    refactor for shorter use statements; bump minor

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 //! A tiny lib crate for tiny dice.
 #![crate_name = "tiny_die"]
 #![no_std]
+use core::fmt;
+use rand::Rng;
 
 #[cfg(test)]
 mod tests {
-    use crate::tiny_die::Die;
+    use crate::Die;
     #[test]
     fn non_zero_roll() {
         // Following can also be written as
@@ -26,64 +28,59 @@ mod tests {
 }
 
 /// Contains the `Die` struct and its functions.
-pub mod tiny_die {
-    use core::fmt;
-    use rand::Rng;
+pub struct Die {
+    /// A die has sides, that's what makes it a die.
+    pub sides: u8,
+}
 
-    pub struct Die {
-        /// A die has sides, that's what makes it a die.
-        pub sides: u8,
+impl Die {
+    /// Returns a die with `sides` number of sides.
+    /// # Arguments
+    ///
+    /// `sides` - optional; a `u8` representing the number of sides on the die
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tiny_die::Die;
+    /// let dee_ten = Die::new(10);
+    /// ```
+    pub fn new(sides: u8) -> Self {
+        Self { sides: sides }
     }
 
-    impl Die {
-        /// Returns a die with `sides` number of sides.
-        /// # Arguments
-        ///
-        /// `sides` - optional; a `u8` representing the number of sides on the die
-        ///
-        /// # Example
-        ///
-        /// ```rust
-        /// use tiny_die::Die;
-        /// let dee_ten = Die::new(10);
-        /// ```
-        pub fn new(sides: u8) -> Self {
-            Self { sides: sides }
-        }
-
-        /// Returns a die with six sides.
-        /// If you just need a standard d6, use Die::default().
-        ///
-        /// # Example
-        ///
-        /// ```rust
-        /// use tiny_die::Die;
-        /// let dee_six = Die::default();
-        /// ```
-        pub fn default() -> Self {
-            // There are two ways this can be executed,
-            // but I haven't timed how long they take either way.
-            // Self { sides: 6 }
-            Self::new(6)
-        }
-
-        /// Return a number between 1 and number of sides plus 1.
-        /// Plus 1 because that's how `gen_range()` works baybee.
-        ///
-        /// # Example
-        /// ```rust
-        /// use tiny_die::Die;
-        /// let dee_ten = Die::new(10);
-        /// let rolled = dee_ten.roll();
-        /// ```
-        pub fn roll(&self) -> u8 {
-            rand::thread_rng().gen_range(1, self.sides + 1)
-        }
+    /// Returns a die with six sides.
+    /// If you just need a standard d6, use Die::default().
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use tiny_die::Die;
+    /// let dee_six = Die::default();
+    /// ```
+    pub fn default() -> Self {
+        // There are two ways this can be executed,
+        // but I haven't timed how long they take either way.
+        // Self { sides: 6 }
+        Self::new(6)
     }
 
-    impl fmt::Display for Die {
-        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-            write!(f, "{}", self.roll())
-        }
+    /// Return a number between 1 and number of sides plus 1.
+    /// Plus 1 because that's how `gen_range()` works baybee.
+    ///
+    /// # Example
+    /// ```rust
+    /// use tiny_die::Die;
+    /// let dee_ten = Die::new(10);
+    /// let rolled = dee_ten.roll();
+    /// ```
+    pub fn roll(&self) -> u8 {
+        rand::thread_rng().gen_range(1, self.sides + 1)
+    }
+}
+
+impl fmt::Display for Die {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.roll())
     }
 }
